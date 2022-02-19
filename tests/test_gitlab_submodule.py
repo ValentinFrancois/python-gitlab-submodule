508d054331cdf4b9ee8df5c333a7381c13dfeeaf
ValentinFrancois <valentin.francois35@gmail.com>
2022-02-19 15:55:17 +0100

    update tests - wip

--- tests/test_gitlab_submodule.py
+++ tests/test_gitlab_submodule.py
@@ -8,52 +8,105 @@ from gitlab_submodule.gitlab_submodule import (list_submodules,
                                                list_subprojects)
 
 
-class TestSubmoduleCommit(unittest.TestCase):
+class TestGitlabSubmodule(unittest.TestCase):
 
-    def test_read_inkscape_subproject(self):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.gl = Gitlab()
 
-        gl = Gitlab()
-        inkscape = gl.projects.get('inkscape/inkscape')
+    def test_read_subproject_details_with_absolute_url(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/gitlab-absolute-urls')
         submodules = list_submodules(
-            inkscape,
-            ref='e371b2f826adcba316f2e64bbf2f697043373d0b')
+            project,
+            ref='ce9b1e50b34372d82df098f3ffded58ef4be03ec')
         submodule = [sub for sub in submodules if
-                     sub.url == 'https://gitlab.com/inkscape/lib2geom.git'][0]
-        submodule_info: Subproject = submodule_to_subproject(submodule, gl)
+                     sub.name == 'git@gitlab.com:fdroid/fdroidclient.git'][0]
+        submodule_info: Subproject = submodule_to_subproject(
+            submodule, self.gl)
         self.assertEqual(submodule_info.submodule, submodule)
 
-        submodule_project = gl.projects.get('inkscape/lib2geom')
+        submodule_project = self.gl.projects.get('fdroid/fdroidclient')
         self.assertEqual(submodule_info.project, submodule_project)
 
         submodule_commit = submodule_project.commits.get(
-            '9d38946b7d7a0486a4a75669008112d306309d9e')
+            'aee2759733857e2d2c021c4f6127f7e8a908a3f2')
         self.assertEqual(submodule_info.commit, submodule_commit)
 
-    def test_list_inkscape_subprojects(self):
-        gl = Gitlab()
-        inkscape = gl.projects.get('inkscape/inkscape')
-        subprojects = list_subprojects(
-            inkscape,
-            gl,
-            ref='e371b2f826adcba316f2e64bbf2f697043373d0b')
+    def test_read_subproject_details_with_relative_url(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/gitlab-relative-urls')
+        submodules = list_submodules(project, ref='main')
+        submodule = [sub for sub in submodules if sub.name == '1'][0]
+        submodule_info: Subproject = submodule_to_subproject(
+            submodule, self.gl)
+        self.assertEqual(submodule_info.submodule, submodule)
+
+        submodule_project = self.gl.projects.get(
+            'python-gitlab-submodule-test/dummy-projects/1')
+        self.assertEqual(submodule_info.project, submodule_project)
+
+        submodule_commit = submodule_project.commits.get(
+            'e5a7153ae32515d9cdba12557345e627f7231fe2')
+        self.assertEqual(submodule_info.commit, submodule_commit)
+
+    def test_list_subprojects_with_absolute_urls(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/gitlab-absolute-urls')
+        subprojects = list_subprojects(project, self.gl)
+        self.assertEqual(3, len(subprojects))
         for subproject in subprojects:
             print('- {} ({}) -> {}'.format(
                 subproject.submodule.path,
                 subproject.project.ssh_url_to_repo,
                 subproject.commit.id))
 
-    def test_compare_inkscape_subprojects_commits_to_head(self):
-        gl = Gitlab()
-        inkscape = gl.projects.get('inkscape/inkscape')
+    def test_list_subprojects_with_relative_urls(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/gitlab-relative-urls')
+        subprojects = list_subprojects(project, self.gl)
+        self.assertEqual(4, len(subprojects))
+        for subproject in subprojects:
+            print('- {} ({}) -> {}'.format(
+                subproject.submodule.path,
+                subproject.project.ssh_url_to_repo,
+                subproject.commit.id))
+
+    def test_list_subprojects_with_external_urls(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/external-urls')
+        subprojects = list_subprojects(project, self.gl)
+        self.assertEqual([], subprojects)
+
+    def test_compare_subprojects_commits_to_head_with_absolute_urls(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/gitlab-absolute-urls')
         subprojects = list_subprojects(
-            inkscape,
-            gl,
-            ref='e371b2f826adcba316f2e64bbf2f697043373d0b')
+            project,
+            self.gl,
+            ref='ce9b1e50b34372d82df098f3ffded58ef4be03ec')
         for subproject in subprojects:
             head_subproject_commit = subproject.project.commits.list(
                 ref=subproject.project.default_branch)[0]
-            up_to_date = subproject.commit.id == head_subproject_commit.id
+            submodule_commit = subproject.commit.id
+            up_to_date = submodule_commit == head_subproject_commit.id
             print('- {}: {}'.format(
                 subproject.submodule.path,
                 'ok' if up_to_date else '/!\\ must update'))
 
+    def test_compare_subprojects_commits_to_head_with_relative_urls(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/gitlab-relative-urls')
+        subprojects = list_subprojects(project, self.gl)
+        for subproject in subprojects:
+            head_subproject_commit = subproject.project.commits.list(
+                ref=subproject.project.default_branch)[0]
+            submodule_commit = subproject.commit.id
+            up_to_date = submodule_commit == head_subproject_commit.id
+            if subproject.name in {'1', '2'}:
+                self.assertTrue(up_to_date)
+            else:
+                self.assertFalse(up_to_date)
+            print('- {}: {}'.format(
+                subproject.submodule.path,
+                'ok' if up_to_date else '/!\\ must update'))
--- tests/test_gitmodules_to_project.py
+++ tests/test_gitmodules_to_project.py
@@ -9,22 +9,45 @@ from gitlab_submodule.submodule_to_project import submodule_to_project
 
 class TestGitmodulesToProject(unittest.TestCase):
 
-    def test_get_inkscape_submodules_as_projects(self):
-        gl = Gitlab()
-        inkscape = gl.projects.get('inkscape/inkscape')
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.gl = Gitlab()
+
+    def test_get_submodules_as_projects_with_gitlab_absolute_urls(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/gitlab-absolute-urls')
         submodules = list_project_submodules(
-            inkscape,
-            ref='e371b2f826adcba316f2e64bbf2f697043373d0b'
-        )
+            project,
+            ref='ce9b1e50b34372d82df098f3ffded58ef4be03ec')
         submodule_projects = [
-            submodule_to_project(submodule, gl.projects)
-            for submodule in submodules
-        ]
+            submodule_to_project(submodule, self.gl.projects)
+            for submodule in submodules]
         self.assertTrue(all(
             isinstance(project, Project)
-            for project in submodule_projects
-        ))
+            for project in submodule_projects))
         self.assertEqual(
-            {'extensions', 'lib2geom', 'themes'},
-            {project.name for project in submodule_projects}
-        )
+            {'Client', 'OpenRGB', 'inkscape'},
+            {project.name for project in submodule_projects})
+
+    def test_get_submodules_as_projects_with_gitlab_relative_urls(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/gitlab-relative-urls')
+        submodules = list_project_submodules(project, ref='main')
+        submodule_projects = [
+            submodule_to_project(submodule, self.gl.projects)
+            for submodule in submodules]
+        self.assertTrue(all(
+            isinstance(project, Project)
+            for project in submodule_projects))
+        self.assertEqual(
+            {'1', '2', '3', '4'},
+            {project.name for project in submodule_projects})
+
+    def test_get_submodules_as_projects_with_external_urls(self):
+        project = self.gl.projects.get(
+            'python-gitlab-submodule-test/test-projects/external-urls')
+        submodules = list_project_submodules(project)
+        self.assertEqual(3, len(submodules))
+        for submodule in submodules:
+            with self.assertRaises(ValueError):
+                submodule_to_project(submodule, self.gl.projects)
