5c341976c24a6c2791749d9ec66887e26ae2e8c2
ValentinFrancois <24223132+ValentinFrancois@users.noreply.github.com>
2022-04-07 13:24:51 +0200

    fix

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
+with open('README.md', 'r') as readme:
+    readme_text = readme.read()
+    readme_text = readme_text.split('## Contributing')[0]
+
+
 setup(
     name='python-gitlab-submodule',
     description='python-gitlab-submodule : '
@@ -23,5 +29,8 @@ setup(
     install_requires=[
        'python-gitlab>=3.0.0',
        'giturlparse>=0.10.0'
-    ]
+    ],
+    long_description=readme_text,
+    long_description_content_type='text/markdown',
+    platforms=['any']
 )
